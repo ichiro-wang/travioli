881e2bf1590726616368dc930bf9f2ccc79ff4d1
Ichiro Wang <ichiwang27@gmail.com>
2025-07-13 21:27:29 -0700

    updated scripts for db migration and echo color

---
+++ scripts/test-migrate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# this script builds the containers for the test environment
+
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+DOCKER_COMPOSE_FILE="docker-compose.test.yml"
+BACKEND_SERVICE="travioli-backend-test"
+
+echo -e "${CYAN}Building containers...${NC}"
+docker compose -f "$DOCKER_COMPOSE_FILE" up --build -d
+
+echo -e "${CYAN}Generating Prisma client and pushing database schema...${NC}"
+docker exec -it "$BACKEND_SERVICE" sh -c "npx prisma generate && npx prisma db push"
+
+echo -e "${GREEN}Test environment is ready!${NC}"
+
+echo -e "${YELLOW}Composing down...${NC}"
+docker compose -f "$DOCKER_COMPOSE_FILE" down
